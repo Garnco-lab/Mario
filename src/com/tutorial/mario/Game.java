e79de0c48f16e4c91b138a770f3fa541fe025938
Steven Deleon <51217148+Garnco-lab@users.noreply.github.com>
2022-03-14 05:43:05 -0700

    main window now opens

--- src/com/tutorial/mario/Game.java
+++ src/com/tutorial/mario/Game.java
@@ -1,5 +1,34 @@
 package com.tutorial.mario;
 
-public class Game {
+import java.awt.Canvas;
+import java.awt.Dimension;
 
+import javax.swing.JFrame;
+
+public class Game extends Canvas {
+
+	public static final int WIDTH = 270;
+	public static final int HEIGHT = WIDTH/14*10;
+	public static final int SCALE = 4;
+	public static final String TITLE = "Mario";
+	
+	public Game() {
+		Dimension size = new Dimension(WIDTH*SCALE,HEIGHT*SCALE);
+		setPreferredSize(size);
+		setMaximumSize(size);
+		setMinimumSize(size);
+	}
+	
+	public static void main(String[] args) {
+		Game game = new Game();
+		JFrame frame = new JFrame(TITLE);
+		frame.add(game);
+		frame.pack();
+		frame.setResizable(false);
+		frame.setLocationRelativeTo(null);
+		frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+		
+	}
+	
 }
