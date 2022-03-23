67f460c2e9618b2cfae8e45cb923d2710b3c1c8f
Steven Deleon <51217148+Garnco-lab@users.noreply.github.com>
2022-03-23 00:43:02 -0700

    init

--- src/com/tutorial/mario/Game.java
+++ src/com/tutorial/mario/Game.java
@@ -5,18 +5,53 @@ import java.awt.Dimension;
 
 import javax.swing.JFrame;
 
-public class Game extends Canvas {
+public class Game extends Canvas implements Runnable {
 
 	public static final int WIDTH = 270;
 	public static final int HEIGHT = WIDTH/14*10;
 	public static final int SCALE = 4;
 	public static final String TITLE = "Mario";
 	
+	private Thread thread;
+	private boolean running = false;
+	
 	public Game() {
 		Dimension size = new Dimension(WIDTH*SCALE,HEIGHT*SCALE);
 		setPreferredSize(size);
 		setMaximumSize(size);
 		setMinimumSize(size);
+	}
+	
+	private synchronized void start() {
+		if(running) return;
+		running = true;
+		thread = new Thread(this,"Thread");
+		thread.start();
+	}
+	
+	public synchronized void stop() {
+		if(!running) return;
+		running = false;
+		try {
+			thread.join();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void run() {
+		while (running) {
+			render();
+			tick();
+		}
+	}
+	
+	public void render() {
+		
+	}
+	
+	public void tick() {
+		
 	}
 	
 	public static void main(String[] args) {
